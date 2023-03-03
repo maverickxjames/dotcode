5f0f7c58cee61d9dce2b3e7292ab58dfeb654e84
Jaykishan <jksonu1436@gmail.com>
2023-03-03 15:47:50 +0530

    Contact Page Updated

--- index.php
+++ index.php
@@ -6,8 +6,7 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0,user-scalable=no">
   <title>Dotcode - Edification</title>
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css"
-    integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
   <link rel="stylesheet" href="./includes/navbar.css">
   <link rel="stylesheet" href="./includes/icons/css/all.css">
   <link rel="stylesheet" href="./includes/styl.css">
@@ -16,81 +15,8 @@
 </head>
 
 <body>
-  <header style="z-index:20">
-    <nav>
-      <input type="checkbox" id="show-search">
-      <input type="checkbox" id="show-menu">
-      <label for="show-menu" class="menu-icon"><i class="fas fa-bars"></i></label>
-      <div class="content">
-        <div class="logo"><a href="#0" target="_blank"><img width="150" src="./includes/img/logo.jpg" alt="logo"
-              srcset=""></a></div>
-        <ul class="links" style="margin-top: 12px;">
-          <li><a href="#">Home</a></li>
-          <li><a href="#About">About</a></li>
-          <li>
-            <a href="#Courses" class="desktop-link">Courses</a>
-            <input type="checkbox" id="show-courses">
-            <label for="show-courses">Services</label>
-            <ul>
-              <li>
-                <a href="#" class="desktop-link">Development</a>
-                <ul>
-                  <li><a href="#">Android</a></li>
-                  <li><a href="#">Java</a></li>
-                  <li><a href="#">PHP</a></li>
-                  <li><a href="#">Bootstrap</a></li>
-                  <li><a href="#">Javascript</a></li>
-                  <li><a href="#">React</a></li>
-                </ul>
-              </li>
-              <li>
-                <a href="#" class="desktop-link">Academics</a>
-                <ul>
-                  <li><a href="#">BCA</a></li>
-                  <li><a href="#">MCA</a></li>
-                  <li><a href="#">PGDCA</a></li>
-                  <li><a href="#">DCA</a></li>
-                  <li><a href="#">11th CS</a></li>
-                  <li><a href="#">12th CS</a></li>
-                </ul>
-              </li>
-              <li>
-                <a href="#" class="desktop-link">Programming</a>
-                <ul>
-                  <li><a href="#">C/C++</a></li>
-                  <li><a href="#">Python</a></li>
-                  <li><a href="#">Java</a></li>
-                  <li><a href="#">VB, VB.NET</a></li>
-                  <li><a href="#">PHP</a></li>
-                  <li><a href="#">SQL</a></li>
-                </ul>
-              </li>
-              <li>
-                <a href="#" class="desktop-link">More Items</a>
-                <ul>
-                  <li><a href="#">MS Office</a></li>
-                  <li><a href="#">Typing</a></li>
-                  <li><a href="#">Web Tech</a></li>
-                  <li><a href="#">Lang</a></li>
-                  <li><a href="#">Networking</a></li>
-                  <li><a href="#">Hardware</a></li>
-                </ul>
-              </li>
-            </ul>
-          </li>
 
-          <li><a href="#">Testimonial</a></li>
-          <li><a href="#">Contact</a></li>
-
-        </ul>
-      </div>
-      <label for="show-search" class="search-icon"><i class="fas fa-search"></i></label>
-      <form action="#" class="search-box">
-        <input type="text" placeholder="Type Something to Search..." required>
-        <button type="submit" class="go-icon"><i class="fas fa-long-arrow-alt-right"></i></button>
-      </form>
-    </nav>
-  </header>
+  <?php include_once('./includes/header.php') ?>
   <div id="carouselExampleIndicators" style="top:70px" class="carousel slide" data-ride="carousel">
     <ol class="carousel-indicators">
       <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
